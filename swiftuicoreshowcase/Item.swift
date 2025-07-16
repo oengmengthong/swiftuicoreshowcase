d55f3e69a1cd606aa056bebae81f96338c330717
Oeng Mengthong <mengthongoenggl@gmail.com>
2025-07-16 15:50:41 +0700

    Initial Commit

---
+++ swiftuicoreshowcase/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  swiftuicoreshowcase
+//
+//  Created by Mengthong on 16/7/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ swiftuicoreshowcaseTests/swiftuicoreshowcaseTests.swift
@@ -0,0 +1,16 @@
+//
+//  swiftuicoreshowcaseTests.swift
+//  swiftuicoreshowcaseTests
+//
+//  Created by Mengthong on 16/7/25.
+//
+
+import Testing
+
+struct swiftuicoreshowcaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
